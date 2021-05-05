745537a504f83a90f48ee60f193898c54d540c3a
Astolffo <82520215+Astolffo@users.noreply.github.com>
2021-05-05 12:16:30 +0300

    Nou cu HD

--- Proiect-LP/proiect.py
+++ Proiect-LP/proiect.py
@@ -1,26 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.primariatm.ro/urbanism/certificate-de-urbanism/"
+url = "https://www.primariahunedoara.ro/ro/info-util/urbanism-cu-si-ac"
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'html.parser')
+html = BeautifulSoup(page.text, 'html.parser')
 
-results = soup.find_all("div", class_= 'card-body')
+titlu = html.find_all('h4')
+context = html.find_all('p')
 
-for results in results:
-        print(results, end="\n"*2)
+for titlu in titlu:
+    print(titlu, end="\n"*2)
 
-for results in results:
-        nrSdate = results.find("p", class_="mb-0 card-title")
-                #nrSdate = numar si date
-        beneficiar = results.find("p", class_="mb-1 card-text")
-        adresa = results.find("p", class_="mb-1 card-text")
-
-        if None in (nrSdate, beneficiar, adresa):
-                continue
-
-        print(nrSdate)
-        print(beneficiar)
-        print(adresa)
-        print()
+for context in context:
+    print(context, end="\n"*2)
