e6e6518339cbf06330b617a414d7d1ea611a919b
Astolffo <82520215+Astolffo@users.noreply.github.com>
2021-05-06 10:56:00 +0300

    Nou cu TM

--- Proiect-LP/proiect.py
+++ Proiect-LP/proiect.py
@@ -1,3 +1,4 @@
+import os
 import urllib
 import urllib.request
 from bs4 import BeautifulSoup
@@ -7,6 +8,16 @@ def make_soup(url):
     soupdata = BeautifulSoup(thepage, "html.parser")
     return soupdata
 
+dateCU = dateCUsaved =  ""
 soup = make_soup("https://xpage.primariatm.ro/lotusweb.nsf/certificateurbanism.xsp")
 for record in soup.findAll('tr'):
-    print(record.text)
+    for data in record.findAll('td'):
+        dateCU = dateCU + ", " + data.text
+    if len(dateCU) != 0:
+        dateCUsaved = dateCUsaved + "\n" + dateCU[1:]
+print(dateCUsaved)
+
+header = "Nr./Data CU, Data identificare, Beneficiar, Adresa, Descriere"
+file = open(os.path.expanduser("CertificateUrbanism.csv"),"wb")
+file.write(bytes(header, encoding="ascii", errors='ignore'))
+file.write(bytes(dateCUsaved, encoding="ascii", errors='ignore'))
