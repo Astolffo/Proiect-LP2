4068c53b1168f95ed22ff1485a5fac2a025858af
Astolffo <82520215+Astolffo@users.noreply.github.com>
2021-05-25 15:54:17 +0300

    Nou cu TM am incercat sa mai fac la tabel Deja ma facu pachet de nervi

--- Proiect-LP/incercari.py
+++ Proiect-LP/incercari.py
@@ -4,6 +4,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 
+
 def make_soup(url):
     thepage = urllib.request.urlopen(url)
     soupdata = BeautifulSoup(thepage, "html.parser")
@@ -11,48 +12,36 @@ def make_soup(url):
 
 soup = make_soup("https://xpage.primariatm.ro/lotusweb.nsf/certificateurbanism.xsp")
 
-NDCU = []
-#Nr./Data CU
-DII = []
-#Date identificare imobil
-Beneficiar = []
-Adresa = []
-Descriere = []
 
 table = soup.find("table",{"class" : "xspDataTable"})
-dateCU = dateCUsaved = ""
+dateCU = ""
 for record in table.find_all("tr"):
     for data in record.findAll("td"):
         newData = data.text.replace("\t", " ").replace("\n", " ").replace("\r", " ")
         dateCU = dateCU + newData + "\t"
     dateCU = dateCU + "\n"
-#print(dateCU)
 
-#file = open("CertificateUrbanismIncercari.csv", "w", encoding='utf8')
-#file.write(dateCU)
-#file.close()
+
+file = open("CertificateUrbanismIncercari.csv", "w", encoding='utf8')
+file.write(dateCU)
+file.close()
 
 Date = str(dateCU)
 
-#for container in Date:
+
 test_str = Date
 regex = r"(\w{3,} nr.\d+|\w{3,}  nr.\d+|\w+ \w{3,} nr.\d+ ap.\d+|\w{3,} \w+\t nr,\d+ bl.- sc.-et.- ap.\d+|\w+ nr.\d\w|\w{3,} nr.\d+ ap.\d+| \w+ \w+ nr.\d+ ap.\d+)"
 dateAdresa = re.finditer(regex,test_str)
 
 for nrCautari, adresaGasita in enumerate(dateAdresa,start = 1):
-    print("S-au gasit {nrCautari} ,acestea fiind : {adresaGasita}".format(nrCautari=nrCautari,adresaGasita=adresaGasita.group()))
+    adrese = "Adresa:{adresaGasita}".format(adresaGasita=adresaGasita.group())
 
-    locatie = str(adresaGasita)
 
-    file1 = open("adrese.txt","w",encoding='utf8')
+
+    locatie = str(adrese)
+
+    file1 = open("adrese.csv","w",encoding='utf8')
     file1.write(locatie)
-    citire = open("adrese.txt","r",encoding='utf8')
-    print(citire)
+    citire = open("adrese.csv","r",encoding='utf8')
+    #print(citire)
 
-#    Adresa.append(dateAdresa)
-
-
-
-file = open("CUIncercari.csv", "w", encoding='utf8')
-file.write(dateCUsaved)
-file.close()
--- Proiect-LP/proiect.py
+++ Proiect-LP/proiect.py
@@ -4,20 +4,16 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 
+import streamlit as st
+
+
+
 def make_soup(url):
     thepage = urllib.request.urlopen(url)
     soupdata = BeautifulSoup(thepage, "html.parser")
     return soupdata
 
 soup = make_soup("https://xpage.primariatm.ro/lotusweb.nsf/certificateurbanism.xsp")
-
-NDCU = []
-#Nr./Data CU
-DII = []
-#Date identificare imobil
-Beneficiar = []
-Adresa = []
-Descriere = []
 
 table = soup.find("table",{"class" : "xspDataTable"})
 dateCU = dateCUsaved = ""
@@ -26,7 +22,7 @@ for record in table.find_all("tr"):
         newData = data.text.replace("\t", " ").replace("\n", " ").replace("\r", " ")
         dateCU = dateCU + newData + "\t"
     dateCU = dateCU + "\n"
-#print(dateCU)
+
 
 file = open("CertificateUrbanismIncercari.csv", "w", encoding='utf8')
 file.write(dateCU)
@@ -34,25 +30,20 @@ file.close()
 
 Date = str(dateCU)
 
-#for container in Date:
+
 test_str = Date
 regex = r"(\w{3,} nr.\d+|\w{3,}  nr.\d+|\w+ \w{3,} nr.\d+ ap.\d+|\w{3,} \w+\t nr,\d+ bl.- sc.-et.- ap.\d+|\w+ nr.\d\w|\w{3,} nr.\d+ ap.\d+| \w+ \w+ nr.\d+ ap.\d+)"
 dateAdresa = re.finditer(regex,test_str)
 
 for nrCautari, adresaGasita in enumerate(dateAdresa,start = 1):
-    print("S-au gasit {nrCautari} ,acestea fiind : {adresaGasita}".format(nrCautari=nrCautari,adresaGasita=adresaGasita.group()))
+    #print("S-au gasit {nrCautari} ,acestea fiind : {adresaGasita}".format(nrCautari=nrCautari,adresaGasita=adresaGasita.group()))
 
-    locatie = str(adresaGasita)
+    locatie = str(adresaGasita.group())
 
     file1 = open("adrese.txt","w",encoding='utf8')
     file1.write(locatie)
     citire = open("adrese.txt","r",encoding='utf8')
     print(citire)
 
-#    Adresa.append(dateAdresa)
-
-
-
-#file = open("CUIncercari.csv", "w", encoding='utf8')
-#file.write(dateCUsaved)
-#file.close()
+#st.image("geocoding.jpg")
+#st.title("Geocoding Application in Python")
