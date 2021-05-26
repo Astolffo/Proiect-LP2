f4065c250bc5347484e287396ff8b54363edb0a8
Astolffo <82520215+Astolffo@users.noreply.github.com>
2021-05-26 15:51:04 +0300

    Nou cu TM am incercat sa mai fac la tabel Deja ma facu pachet de nervi

--- Proiect-LP/proiect.py
+++ Proiect-LP/proiect.py
@@ -1,7 +1,17 @@
 import re
 import urllib
 import urllib.request
+from urllib import request
+
+import requests
+import self as self
 from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+import pandas as pd
+
+
+import streamlit as st
+
 
 
 def make_soup(url):
@@ -11,14 +21,6 @@ def make_soup(url):
 
 soup = make_soup("https://xpage.primariatm.ro/lotusweb.nsf/certificateurbanism.xsp")
 
-NDCU = []
-#Nr./Data CU
-DII = []
-#Date identificare imobil
-Beneficiar = []
-Adresa = []
-Descriere = []
-
 table = soup.find("table",{"class" : "xspDataTable"})
 dateCU = dateCUsaved = ""
 for record in table.find_all("tr"):
@@ -26,7 +28,7 @@ for record in table.find_all("tr"):
         newData = data.text.replace("\t", " ").replace("\n", " ").replace("\r", " ")
         dateCU = dateCU + newData + "\t"
     dateCU = dateCU + "\n"
-#print(dateCU)
+
 
 file = open("CertificateUrbanismIncercari.csv", "w", encoding='utf8')
 file.write(dateCU)
@@ -34,25 +36,82 @@ file.close()
 
 Date = str(dateCU)
 
-#for container in Date:
+
 test_str = Date
 regex = r"(\w{3,} nr.\d+|\w{3,}  nr.\d+|\w+ \w{3,} nr.\d+ ap.\d+|\w{3,} \w+\t nr,\d+ bl.- sc.-et.- ap.\d+|\w+ nr.\d\w|\w{3,} nr.\d+ ap.\d+| \w+ \w+ nr.\d+ ap.\d+)"
 dateAdresa = re.finditer(regex,test_str)
 
 for nrCautari, adresaGasita in enumerate(dateAdresa,start = 1):
-    print("S-au gasit {nrCautari} ,acestea fiind : {adresaGasita}".format(nrCautari=nrCautari,adresaGasita=adresaGasita.group()))
+    #print("S-au gasit {nrCautari} ,acestea fiind : {adresaGasita}".format(nrCautari=nrCautari,adresaGasita=adresaGasita.group()))
 
-    locatie = str(adresaGasita)
+    locatie = str(adresaGasita.group())
 
     file1 = open("adrese.txt","w",encoding='utf8')
     file1.write(locatie)
     citire = open("adrese.txt","r",encoding='utf8')
     print(citire)
 
-#    Adresa.append(dateAdresa)
+
+mapbox_access_token = open("adrese.csv").read()
+
+s = requests.Session()
+
+response_csv = s.get(dateCU)
+adapter = self.get_adapter(url=request.url)
+
+df = pd.get(adapter)
+site_lat = df.lat
+site_lon = df.lon
+locations_name = df.text
+
+fig = go.Figure()
+
+fig.add_trace(go.Scattermapbox(
+        lat=site_lat,
+        lon=site_lon,
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=17,
+            color='rgb(255, 0, 0)',
+            opacity=0.7
+        ),
+        text=locations_name,
+        hoverinfo='text'
+    ))
+
+fig.add_trace(go.Scattermapbox(
+        lat=site_lat,
+        lon=site_lon,
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=8,
+            color='rgb(242, 177, 172)',
+            opacity=0.7
+        ),
+        hoverinfo='none'
+    ))
+
+fig.update_layout(
+    title='Certificate Urbanism',
+    autosize=True,
+    hovermode='closest',
+    showlegend=False,
+    mapbox=dict(
+        accesstoken=mapbox_access_token,
+        bearing=0,
+        center=dict(
+            lat=38,
+            lon=-94
+        ),
+        pitch=0,
+        zoom=3,
+        style='light'
+    ),
+)
+
+fig.show()
 
 
 
-#file = open("CUIncercari.csv", "w", encoding='utf8')
-#file.write(dateCUsaved)
-#file.close()
+#st.image("geocoding.jpg")
+#st.title("Geocoding Application in Python")
